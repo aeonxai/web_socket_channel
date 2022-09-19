ee71338fd89a5319a933da971b25adf92bf9c6fa
Marzuk Kamal <marzuk9999@gmail.com>
2022-09-19 12:55:04 +0200

    update

--- lib/src/_connect_io.dart
+++ lib/src/_connect_io.dart
@@ -11,5 +11,12 @@ import '../web_socket_channel.dart';
 /// communicate over the resulting socket.
 ///
 /// The optional [protocols] parameter is the same as `WebSocket.connect`.
-WebSocketChannel connect(Uri uri, {Iterable<String>? protocols, Duration? pingInterval}) =>
-    IOWebSocketChannel.connect(uri, protocols: protocols, pingInterval: pingInterval);
+WebSocketChannel connect(Uri uri,
+    {Iterable<String>? protocols, Duration? pingInterval}) {
+  if (pingInterval != null) {
+    return IOWebSocketChannel.connect(uri,
+        protocols: protocols, pingInterval: pingInterval);
+  } else {
+    return IOWebSocketChannel.connect(uri, protocols: protocols);
+  }
+}
